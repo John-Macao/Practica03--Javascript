a3f0045d70f5d3b9033877f9da0ce93676678133
Jhon-Macao <jhonmacao@gmail.com>
2020-05-10 14:25:31 -0500

    añadiendo funcionamiento a la calculadora

--- Calculadora/controlador.js
+++ Calculadora/controlador.js
@@ -1,1 +1,19 @@
+function datos(num){
+    var anterior = document.getElementById("valores").value+=num;
+}
+
+function borrar_todo(){
+    document.getElementById("valores").value="";
+}
+
+function calcular(){
+    var resultado = eval(document.getElementById("valores").value);
+
+    if(resultado=="Infinity"){
+        document.getElementById("valores").value="No se puede dividir entre cero";
+    }else{
+        document.getElementById("valores").value=resultado;
+    }
     
+}
+
