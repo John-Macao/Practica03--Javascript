df5fe801a34fe24778e477fd14da953afec6e09b
Jhon-Macao <jhonmacao@gmail.com>
2020-05-12 17:46:51 -0500

    final de galeria

--- Galeria_imagenes/controlImagenes.js
+++ Galeria_imagenes/controlImagenes.js
@@ -1,81 +1,156 @@
 
-      //seleccionar imágenes
-      var coche1 = document.getElementById("coche1");
-      var coche2 = document.getElementById("coche2");
+var misFotos=["<img src='imagenes/1.jpg'>",
+        "<img src='imagenes/2.jpg'>" ,
+        "<img src='imagenes/3.jpg'>" ,
+        "<img src='imagenes/4.jpg'>" ,
+        "<img src='imagenes/5.jpg'>" ,
+        "<img src='imagenes/6.jpg'>" ,
+        "<img src='imagenes/7.jpg'>",
+        "<img src='imagenes/8.jpg'>",
+        "<img src='imagenes/9.jpg'>",
+        "<img src='imagenes/10.jpg'>" ];
 
-      //posiciones iniciales
-      var x_coche1 = 0;
-      var x_coche2 = 0;
+var verConsola =muestra=0; 
+nodo = 0;
 
-      function correr() {
-      	   //movimiento coche1
-	   if (x_coche1 <= 85 && x_coche1 >= 0) { //condición para que se mueva entre estos valores de la pantalla
-		x_coche1 = x_coche1 + 2; //movimiento del valor x
-		coche1.style.left = x_coche1 + "%"; //aplicar el valor obtenido al left del elemento
-	    }
-	    else { //si no cumple la condición, es decir, se sale de los valores de la pantalla, vuelve a empezar
-		 x_coche1=0; //posicion inicial
-		 coche1.style.left = x_coche1 + "%"; //volvemos a aplicar el valor obtenido al left del elemento
-	    }
+window.onload = function() { 
 
-	    //movimiento coche2
-            if (x_coche2 <= 85 && x_coche2 >= 0) { //condición para que se mueva entre estos valores de la pantalla
-		 x_coche2 = x_coche2 + 1; //movimiento del valor x
-		 coche2.style.left = x_coche2 + "%"; //aplicar el valor obtenido al left del elemento
-	    }
-            else { //si no cumple la condición, es decir, se sale de los valores de la pantalla, vuelve a empezar
-		 x_coche2=0; //posicion inicial
-		 coche2.style.left = x_coche2 + "%"; //volvemos a aplicar el valor obtenido al left del elemento
-	    }
-      }
+foto=document.getElementById("fotoSale"); //elemento foto saliente
+fotoAnt=document.getElementById("fotoEntra"); //elemento foto entrante
 
+console.log(verConsola);
+document.getElementById("botonretro").disabled=true;
+document.getElementById("botonpara").disabled=true;
+//document.getElementById("botoninicia").disabled=true;
 
+var pararCambio=setInterval(cambio,8000)
+}
 
+function mueve(opcion) {
+anterior=misFotos[muestra]; 
+fotoAnt.innerHTML=anterior;
 
+switch (opcion) { 
+    case "avance":
+        var aa = muestra++;
+        console.log(aa);
+        if (muestra>3) { 
+            document.getElementById("botonAvanza").disabled=true;
+        }else{
+            document.getElementById("botonretro").disabled=false;
+        }
+        estilo="derecho";
+    break;
 
-
-
-
-/*
-var p = 0;
-var ah = 5;
-var av = 5;
-
-limD = document.body.clientWidth -100;
-limI = 0;
-
-limSu = 0;
-limIn = document.body.clientHeight -100;
-
-
-function mueve() {
-
-    var capa = document.getElementById('fotoSale');
-    ph = parseInt(capa.style.left);
-    pv = parseInt(capa.style.top);
-    if(ph < limI){
-        ah = 5;
-    }
-    if(ph > limD){
-        ah = -5;
-    }
-    if(pv < limSu){
-        av = 5;
-    }
-    if(pv > limIn){
-        av = -5;
-    }
-
-    ph = ph + ah;
-    capa.style.left = ph + "px";
-    pv = pv + av;
-    capa.style.top = pv + "px";
+    case "retro":
+        document.getElementById("botonAvanza").disabled=false;
+        var aa = muestra--;
+        console.log(aa);
+        if (muestra <= 0) {
+            document.getElementById("botonretro").disabled=true;
+        }
+        estilo="izquierdo"
+    break;
 
 }
 
-*/
+ver=misFotos[muestra]; 
+mueveFoto = 400; 
+mueveFoto2=-400; 
+completado=setInterval(movimientoFoto,25);
+}
 
 
+function mueve2() {
+    console.log("1");
+
+   // clearInterval(completado) //parar temporizador de transición
+   // clearInterval(pararCambio) //parar temporizador cambio de imagen
+    pararC=setInterval(comprobar,8000) //iniciar temporizador cambio de imagen
+    comprobar() //cambiar imagen.
+
+
+    //mac = setInterval(comprobar,4000);
+    //clearInterval(mac);
+}
+
+function comprobar(){
+    
+    anterior=misFotos[muestra]; 
+fotoAnt.innerHTML=anterior;
+
+estilo="iniciar";
+ver=misFotos[muestra];
+mueveFoto = 400;
+mueveFoto2=-400;
+
+completado=setInterval(movimientoFoto,50);
+
+muestra=muestra+1;
+
+if (muestra > 4) { //reiniciar tras llegar al final.
+    muestra=0;	
+    }
+    
+}
+
+
+
+function movimientoFoto() { 
+if (estilo=="derecho") {
+  mueveFoto-=10;
+  mueveAnt=mueveFoto-400; 
+  cambioFoto=mueveFoto+"px"; 
+  cambioAnt=mueveAnt+"px";
+  foto.style.left=cambioFoto; 
+  fotoAnt.style.left=cambioAnt;
+  foto.innerHTML=ver;
+  if (mueveFoto<=0) { 
+     parar() 
+     }
+  }
+
+else if (estilo=="izquierdo") {  		
+  mueveFoto2+=10;
+  mueveAnt=mueveFoto2+400;
+  cambioFoto=mueveFoto2+"px";
+  cambioAnt=mueveAnt+"px";
+  foto.style.left=cambioFoto;
+  fotoAnt.style.left=cambioAnt;
+  foto.innerHTML=ver;
+  if (mueveFoto2>=0) {
+     parar()
+     }
+  }
+
+  else if (estilo == "iniciar") {
+        mueveFoto-=10; 
+      mueveAnt=mueveFoto-400; 
+      cambioFoto=mueveFoto+"px"; 
+      cambioAnt=mueveAnt+"px";
+      foto.style.left=cambioFoto; 
+      fotoAnt.style.left=cambioAnt;
+      foto.innerHTML=ver;
+      if (mueveFoto<=0) { 
+        parar() 
+        }
+     }
+     
+}
+
+function parar() {
+clearInterval(completado);
+ //numff=muestra+1; //colocamos la nueva foto como fondo del visor.
+ //fotoFondo="foto"+numff+".jpg"
+ //imagenFondo="url("+fotoFondo+")";
+ //pantalla.style.backgroundImage=imagenFondo
+}
+
+function parar2() {
+    clearInterval(pararC);
+    
+    }
+    
 
 
 
