dd3a7a73ae22817dbb00bcf3bd20da906306c25f
Jhon-Macao <jhonmacao@gmail.com>
2020-05-13 05:30:56 -0500

    ultima funcionalidad a formulario

---
+++ Validacion_formulario/mensaje.php
@@ -0,0 +1,18 @@
+<html> 
+<head> 
+    <meta charset="UTF-8"> 
+    <title>PHP.</title>
+</head> 
+
+<body>
+    <?php 
+
+    $saludo= "Bienvenido";
+
+    echo "$saludo";
+    
+    ?>
+
+
+</body>
+</html>
